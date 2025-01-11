42ca7861e9fc2c4ac68d2aa3d9b4dae45a15908e
defaultplayer2 <iprocom90@gmail.com>
2025-01-11 20:03:43 +0700

    Add files via upload

---
+++ Final1.py
@@ -0,0 +1,21 @@
+name = str(input("Введите ваше имя: "))
+created_date = str(input("Введите дату создания в формате дд/мм/гггг: "))
+issue_date = str(input("Введите дату изменений в формате дд/мм/гггг: "))
+status1 = str(input("Введите статус задачи: "))
+title1 = str(input("Введите заголовок заметки: "))
+title2 = str(input("Введите заголовок заметки: "))
+title3 = str(input("Введите заголовок заметки: "))
+content1  = str(input("Введите вашу заметку: "))
+
+title_list1 = [ title1, title2, title3]
+
+note = [
+"Имя пользователя: ", name,
+"Содержание заметки: ", content1,
+"Статус: ", status1,
+"Дата создания: ", created_date,
+"Дата изменения: ", issue_date,
+"Заголовки: ", title_list1
+]
+
+print(note)
